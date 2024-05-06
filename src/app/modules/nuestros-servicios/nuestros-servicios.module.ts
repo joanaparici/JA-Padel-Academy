f02d76f44d4d945defc4fc9ecbf2ed108ebdb8a3
Joan Aparici <joanaparici22@gmail.com>
2024-05-06 09:40:19 +0200

    (feature/nuestrosServicios) Added nuestrosServicios model and his main-page.

--- src/app/modules/nuestros-servicios/nuestros-servicios.module.ts
+++ src/app/modules/nuestros-servicios/nuestros-servicios.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NuestrosServiciosRoutingModule } from './nuestros-servicios-routing.module';
-
+import { MainPageComponent } from './main-page/main-page.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    NuestrosServiciosRoutingModule
-  ]
+  declarations: [MainPageComponent],
+  imports: [CommonModule, NuestrosServiciosRoutingModule, MatCardModule],
 })
-export class NuestrosServiciosModule { }
+export class NuestrosServiciosModule {}
