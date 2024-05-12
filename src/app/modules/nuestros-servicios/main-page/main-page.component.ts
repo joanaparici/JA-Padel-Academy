0aafb8718541d086d71593245d64a3a8af2d67cd
Joan Aparici <joanaparici22@gmail.com>
2024-05-12 09:12:02 +0200

    (feature/services-animations) Finished charge animation

--- src/app/modules/nuestros-servicios/main-page/main-page.component.ts
+++ src/app/modules/nuestros-servicios/main-page/main-page.component.ts
@@ -1,10 +1,29 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ServicesService } from 'src/app/services/ourServices/services.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-15px)' }),
+        animate(
+          '850ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0px)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class MainPageComponent implements OnInit {
   services: any[] = [];
@@ -12,7 +31,7 @@ export class MainPageComponent implements OnInit {
   constructor(private servicesService: ServicesService) {}
 
   ngOnInit() {
-    this.servicesService.getServices().subscribe(data => {
+    this.servicesService.getServices().subscribe((data) => {
       this.services = data;
     });
   }
